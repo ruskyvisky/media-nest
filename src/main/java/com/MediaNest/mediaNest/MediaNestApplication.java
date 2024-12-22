08ea9ca61db68ef6101a181e1752b74d59bb305e
ruskyvisky <numan_859@hotmail.com>
2024-12-22 19:54:47 +0300

    first commit

---
+++ src/main/java/com/MediaNest/mediaNest/MediaNestApplication.java
@@ -0,0 +1,13 @@
+package com.MediaNest.mediaNest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaNestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaNestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MediaNest/mediaNest/MediaNestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MediaNest.mediaNest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaNestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
