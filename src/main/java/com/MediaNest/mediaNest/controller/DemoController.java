457a7cd4710d9e20edebe0ddd18a8703577711bc
selçuk yılmaz <selcuuukyilmaz@gmail.com>
2025-01-04 14:30:23 +0300

    ADD : implement DemoController with a demo endpoint

---
+++ src/main/java/com/MediaNest/mediaNest/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.MediaNest.mediaNest.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+
+    @GetMapping("/demo")
+    public ResponseEntity<String> demo() {
+        return ResponseEntity.ok("Hello World");
+    }
+    
+}
