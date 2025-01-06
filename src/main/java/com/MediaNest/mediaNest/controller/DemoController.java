baaf3f15bfb788fc565f01f59041fedb75f9c460
ruskyvisky <numan_859@hotmail.com>
2025-01-07 00:13:24 +0300

    Github rules error  testing

--- src/main/java/com/MediaNest/mediaNest/controller/DemoController.java
+++ src/main/java/com/MediaNest/mediaNest/controller/DemoController.java
@@ -13,7 +13,7 @@ public class DemoController {
     }
 
     @GetMapping("/test")
-    public String test(){
-        return "test";
+    public String test() {
+        return "tesssssss"t;
     }
 }
